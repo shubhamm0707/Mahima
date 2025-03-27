89f85f6464462fb5ee92f879d305324f8ca8b4ee
Skorax <skorax@Skoraxs-MacBook-Air.local>
2025-03-27 18:08:09 +0530

    done

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ window.addEventListener("scroll", function () {
 });
 
 
-document.getElementById("agreeBtn").addEventListener("click", function () {
-    document.getElementById("disclaimer").style.display = "none";
-});
+// document.getElementById("agreeBtn").addEventListener("click", function () {
+//     document.getElementById("disclaimer").style.display = "none";
+// });
+
+
+
+// const text = "Business Law & Compliance";
+// const dynamicElement = document.querySelector(".dynamic");
+// let index = 0;
+// let deleting = false;
+
+// function typeEffect() {
+//     if (!deleting) {
+//         dynamicElement.textContent = text.substring(0, index++);
+//         if (index > text.length) {
+//             deleting = true;
+//             setTimeout(typeEffect, 1000); // Pause before deleting
+//             return;
+//         }
+//     } else {
+//         dynamicElement.textContent = text.substring(0, index--);
+//         if (index === 0) {
+//             deleting = false;
+//         }
+//     }
+
+//     setTimeout(typeEffect, deleting ? 50 : 100); // Adjust speed for typing and deleting
+// }
+
+// typeEffect();
